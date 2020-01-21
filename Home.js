12b41817144a147791125c6d090598ab334d1c33
Jared Grimes <jaredlgrimes@hotmail.com>
2020-01-21 12:04:44 -0500

    presentation ready

--- Home.js
+++ Home.js
@@ -1,50 +1,131 @@
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Title, Content, 
-    Text, Form, Item, Label, Input, Button, Icon, Grid, Col } from 'native-base';
-import { View } from 'react-native'
-export default function Home() {
-    return (
-        <View style={{
-                flexDirection: 'row',
-                height: 100,
-                padding: 20,
-            }}>
-            <Container>
-                <Content>
-                    <Body>
-                        <Text>
-                            Enter as many places as you'd like
-                        </Text>
-                    </Body>
-                    
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Place 1</Label>
-                        <Input />
-                        </Item>
-                        <Item floatingLabel last>
-                            <Label>Place 2</Label>
-                            <Input />
-                        </Item>
-                        <Item floatingLabel last>
-                            <Label>Place 3</Label>
-                            <Input />
-                        </Item>
-                    </Form>
+    Text, Form, Item, Label, Input, Button, Icon, Grid, Col, Root, ActionSheet,
+    CheckBox, 
+    Row} from 'native-base';
+import { View, StyleSheet } from 'react-native'
 
-                    <View style={{ width: 150}}>
-                        <Button iconLeft transparent>
-                            <Icon name='add' />
-                            <Text>Add a place</Text>
-                        </Button>
-                    </View>
+var BUTTONS = ["Apple Maps", "Google Maps", "Waze", "Cancel"];
+var CANCEL_INDEX = 3;
 
-                    <Button iconLeft>
-                        <Icon name='search' />
-                        <Text>Find Shortest Path</Text>
-                    </Button>
-                </Content>
-            </Container>
-        </View>
-    );
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+        justifyContent: "space-between"
+    },
+    innerContainer: {
+        flexDirection: 'row',
+        paddingTop: 20,
+        justifyContent: "space-evenly"
+    },
+    title: {
+        fontSize: 44,
+        fontWeight: "400",
+        textAlign: "center"
+    },
+    subtitle: {
+        fontStyle: "italic",
+        fontWeight: "300",
+        textAlign: "center"
+    },
+    header: {
+        fontSize: 32,
+        fontWeight: "400"
+    },
+    explanationText: {
+        textAlign: "center"
+    },
+    text: {
+        textAlign: "center"
+    },
+    titleBlock: {
+        paddingTop: 50,
+        paddingBottom: 30,
+    }
+})
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            returnBackHome: false
+        };
+      }
+    render() {
+        return (
+            <Root>
+                <View style={styles.container}>
+                    <Container>
+                        <Content>
+                            <View style={styles.titleBlock}>
+                                <Text style={styles.title}>
+                                    Cutting Corners
+                                </Text>
+                                <Text style={styles.subtitle}>
+                                    Find the quickest path between all your daily stops
+                                </Text>
+                            </View>
+
+                            <View style={styles.innerContainer}>
+                                 <Text style={styles.header}>
+                                    Enter places
+                                </Text>
+                            </View>
+                            
+                            <Form>
+                                <Item floatingLabel>
+                                    <Label>Place 1</Label>
+                                <Input />
+                                </Item>
+                                <Item floatingLabel last>
+                                    <Label>Place 2</Label>
+                                    <Input />
+                                </Item>
+                                <Item floatingLabel last>
+                                    <Label>Place 3</Label>
+                                    <Input />
+                                </Item>
+                                <Item floatingLabel last>
+                                    <Label>Place 4</Label>
+                                    <Input />
+                                </Item>
+                            </Form>
+
+                            <View style={styles.innerContainer}>
+                                <Button iconLeft transparent>
+                                    <Icon name='add' />
+                                    <Text>Add a place</Text>
+                                </Button>
+                            </View>
+
+                            <View style={styles.innerContainer}>
+                                <CheckBox checked={this.state.returnBackHome} onPress={() => this.setState({returnBackHome: !this.state.returnBackHome})} />
+                                <Text>Go back home at the end of your route</Text>
+                            </View>
+
+                            <View style={styles.innerContainer}>
+                                <Button iconLeft 
+                                    onPress={() =>
+                                        ActionSheet.show(
+                                        {
+                                            options: BUTTONS,
+                                            cancelButtonIndex: CANCEL_INDEX,
+                                            title: "Shortest path generated! Choose an app to open in"
+                                        },
+                                        buttonIndex => {
+                                            this.setState({ clicked: BUTTONS[buttonIndex] });
+                                        }
+                                    )}>
+                                    <Icon name='search' />
+                                    <Text>Find Shortest Path</Text>
+                                </Button>
+                            </View>
+                        </Content>
+                    </Container>
+                </View>
+            </Root>
+        );
+    }
 }
