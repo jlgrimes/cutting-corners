647f2afcdeacc8367ff34acddf315b6ce3f6efb1
Maxim Bartnitski <mbartnitski@Maxims-MacBook-Pro.local>
2020-04-10 00:37:34 -0400

    General locations and permutator

--- Home.js
+++ Home.js
@@ -1,3 +1,7 @@
+// Ideas:
+//      Notification if all results for search query are closed, line 186
+// #SPEED: places to potentially improve speed
+
 import MAPS_API_KEY from './api-key'
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Title, Content, 
@@ -6,7 +10,7 @@ import { Container, Header, Left, Body, Right, Title, Content,
     Row} from 'native-base';
 import { View, StyleSheet, ListItem } from 'react-native'
 import { generateAPIUrl, permutator, generatePathUrl, generatePlaceAutocompleteUrl, 
-    PLACE_TEXT, STARTING_PLACE_TEXT, ENDING_PLACE_TEXT } from './const';
+    PLACE_TEXT, STARTING_PLACE_TEXT, ENDING_PLACE_TEXT, generateGeneralSearchURL } from './const';
 import { Linking } from 'expo';
 import DraggableFlatList from "react-native-draggable-flatlist";
 import { Autocomplete } from 'native-base-autocomplete';
@@ -17,6 +21,8 @@ Geocode.setApiKey(MAPS_API_KEY);
 
 var BUTTONS = ["Apple Maps", "Google Maps", "Waze", "Cancel"];
 var CANCEL_INDEX = 3;
+let CURRENT_ADDRESS = "";
+let CURRENT_COORDS = "42.2745128,-83.7355595";
 
 const styles = StyleSheet.create({
     container: {
@@ -68,10 +74,12 @@ export default class Home extends Component {
         super(props);
         this.state = {
             returnBackHome: false,
-            startAtCurrentLocation: false,
+            // startAtCurrentLocation: false,
             destinations: ["", "", "", "", ""], // initializes three empty places
             allDestinations: ["", "", "", "", ""], // used for distance matrix calc
-            autocomplete: []
+            autocomplete: [],
+            permDestinations: [], // list of [address, type] pairs to accomodate general queries. type = 0 for specific addres, type = 1+ for general address
+            matrixDestinations: [] // list of all possible destinations including all general search results
         };
         this.addPlace = this.addPlace.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
@@ -83,28 +91,10 @@ export default class Home extends Component {
         this.setCurrentLocation();
     }
 
-    getDurationFromPath(distanceMatrix, path) {
-        var duration = 0
-        const { allDestinations } = this.state
-
-        // iterating through all the sequential pairs
-        for (let i = 0; i < path.length - 1; i++) {
-            let firstPoint = allDestinations.indexOf(path[i])
-            let secondPoint = allDestinations.indexOf(path[i + 1])
-
-            let thisDuration = distanceMatrix[firstPoint]["elements"][secondPoint]["duration"]["value"]
-            console.log(path[i] + "==>" + path[i + 1])
-            console.log(thisDuration)
-
-            duration += thisDuration
-        }
-        return duration
-    }
-
     setCurrentLocation() {
         let destinations = this.state.destinations.slice();
 
-        var getPosition = function () {
+        let getPosition = function () {
             var options = {
                 enableHighAccuracy: true,
                 timeout: 10000,
@@ -114,16 +104,17 @@ export default class Home extends Component {
             return new Promise(function (resolve, reject) {
               navigator.geolocation.getCurrentPosition(resolve, reject, options);
             });
-          }
+        };
           
         getPosition()
             .then((pos) => {
-                console.log(pos);
+                CURRENT_COORDS = pos.coords.latitude + "," + pos.coords.longitude;
                 Geocode.fromLatLng(pos.coords.latitude, pos.coords.longitude).then(
                     response => {
                         let street = response.results[0].address_components[0].short_name + " " + response.results[0].address_components[1].short_name;
                         let city = response.results[0].address_components[3].short_name;
                         let address = street + " " + city;
+                        CURRENT_ADDRESS = address;
                         destinations[0] = address;
                         this.setState({ destinations });
                         console.log("Current location retrieved successfully and set\nAddress: " + address);
@@ -138,17 +129,120 @@ export default class Home extends Component {
         });
     }
 
-    bruteForceShortestPath(distanceMatrix) {
-        console.log(distanceMatrix)
-        const { allDestinations } = this.state
+    // determines startpoint coordinates, then calls getGeneralLocations
+    async getGeneralLocations() {
+        let start_coords = CURRENT_COORDS;
+        let start = this.state.destinations[0];
 
-        let startingDestination = allDestinations[0]
-        let endingDestination = allDestinations[allDestinations.length - 1]
-        let destinations = allDestinations.slice(1, allDestinations.length - 1)
-        let permutations = permutator(destinations)
+        // if user changed start from current location
+        // set start_coords to coordinates of the specified start address
+        if (start != CURRENT_ADDRESS) {
+            let result = await Geocode.fromAddress(start);
+            let lat = result["results"][0]["geometry"]["location"]["lat"];
+            let long = result["results"][0]["geometry"]["location"]["lng"];
+            start_coords = lat + "," + long;
+        }
 
-        let minDuration = Number.MAX_SAFE_INTEGER
-        let minPath = []
+        // iterates through destinations excluding start and end, if destination does not have a number consider it general query
+        // url request returns places that match query within ~20 miles (30000 meters) 
+        // of either the specific start address
+        console.log("STARTING GENERAL SEARCH");
+        
+        let type = 1;
+        let numSearch = this.state.allDestinations.length - 2;
+
+        // add start to matrixDestinations to be sent to API
+        this.setState(prevState => ({
+            matrixDestinations: [...prevState.matrixDestinations, start]
+        }));
+
+        // if end != start, add end as well
+        let end = this.state.allDestinations[this.state.allDestinations.length - 1];
+        if (start != end) {
+            this.setState(prevState => ({
+                matrixDestinations: [...prevState.matrixDestinations, end]
+            }));
+        }
+
+        // generate permDests and matrixDests
+        for (let i = 1; i < this.state.allDestinations.length - 1; i++) {            
+
+            // if dest has number, then its specific
+            if (/\d/.test(this.state.allDestinations[i])) {
+                // add to matrixDestinations as is
+                this.setState(prevState => ({
+                    matrixDestinations: [...prevState.matrixDestinations, this.state.allDestinations[i]]
+                }));
+
+                // add to permDestinations with type = 0 because its specific
+                this.setState(prevState => ({
+                    permDestinations: [...prevState.permDestinations, [this.state.allDestinations[i], 0]]
+                }));
+
+            // else general query
+            } else {
+                let query = this.state.allDestinations[i];
+                let url = generateGeneralSearchURL(query, start_coords);
+
+                // url request returns places that match query within ~20 miles (30000 meters) 
+                // of the starting coordinates
+                let response = await fetch(url);
+                let data = await response.json();
+                    
+                let results = data["results"];
+                
+                // arbitrarily set to 3 closest to minimize runtime #SPEED
+                for (let i = 0; i < 3; i++) {
+                    // if (results[i]["opening_hours"]["open_now"]) { #FIX: ignored for testing
+                        // add address to matrix destations
+                        this.setState(prevState => ({
+                            matrixDestinations: [...prevState.matrixDestinations, results[i]["formatted_address"]]
+                        }));
+                        // add [address, type] to permDests
+                        this.setState(prevState => ({
+                            permDestinations: [...prevState.permDestinations, [results[i]["formatted_address"], type]]
+                        }));
+                    // }                    
+
+                }
+                type++;
+            }
+        } 
+        return numSearch;
+    }
+
+    getDurationFromPath(distanceMatrix, path) {
+        var duration = 0
+        const { matrixDestinations } = this.state;
+        
+
+        // iterating through all the sequential pairs
+        for (let i = 0; i < path.length - 1; i++) {
+            let firstPoint = matrixDestinations.indexOf(path[i])
+            let secondPoint = matrixDestinations.indexOf(path[i + 1])
+
+            let thisDuration = distanceMatrix[firstPoint]["elements"][secondPoint]["duration"]["value"]
+            // console.log(path[i] + "==>" + path[i + 1])
+            // console.log(thisDuration)
+
+            duration += thisDuration
+        }
+        return duration
+    }
+
+    bruteForceShortestPath(distanceMatrix, numSearch) {
+        console.log("DISTANCE MATRIX22222: " + distanceMatrix);
+        
+        let startingDestination = this.state.allDestinations[0];
+        let endingDestination = this.state.allDestinations[this.state.allDestinations.length - 1];
+        let destinations = this.state.permDestinations.slice(1, this.state.permDestinations.length - 1);        
+        
+        let permutations = permutator(destinations, numSearch); 
+        console.log("PERMUTATIONS:");
+        console.log(permutations);        
+
+        let minDuration = Number.MAX_SAFE_INTEGER;
+        let minPath = [];
 
         permutations.forEach(perm => {
             var path = perm
@@ -164,9 +258,9 @@ export default class Home extends Component {
             }
         })
 
-        console.log("TOTAL SHORTEST PATH")
-        console.log(minPath.join(" ==> "))
-        console.log(minDuration)
+        // console.log("TOTAL SHORTEST PATH")
+        // console.log(minPath.join(" ==> "))
+        // console.log(minDuration)
 
         let origin = minPath[0]
         let waypoints = minPath.slice(1, minPath.length - 1)
@@ -181,7 +275,7 @@ export default class Home extends Component {
                 title: "Shortest path generated! Choose an app to open in"
             },
             buttonIndex => {
-                console.log(pathUrl)
+                // console.log(pathUrl)
                 if (BUTTONS[buttonIndex] == "Google Maps") {
                     Linking.openURL(pathUrl).catch((err) => console.error('An error occurred', err));
                 }
@@ -190,24 +284,37 @@ export default class Home extends Component {
         )
     }
 
+    // calls distance matrix API with allDestinations
     getAllDistances() {
-        let url = generateAPIUrl(this.state.allDestinations)
-        console.log(url)
+        console.log("ALL DESTINATIONS:\n" + this.state.allDestinations);
+        
+        // find specific addresses for general queries
+        this.getGeneralLocations().then((numSearch) => {
+            console.log('GETTING GENERAL RESULTS FINISHED');
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                //let allDestinations = data["destination_addresses"]
-                //this.setState({ allDestinations })
-                // here we update the allDestinations in the state
-                // this replaces the generic names we type in with the specific addresses
-                // for example, Washington gets replaced with Washington, USA
-                // if we don't want the user interface to change, we can replace this with
-                // a different variable no problem
+            // uses matrixDestinations to return matrix of all distances between all possibilities
+            console.log("MATRIX: " + this.state.matrixDestinations);            
+            let url = generateAPIUrl(this.state.matrixDestinations);
+            console.log(url);
 
-                let distanceMatrix = data["rows"]
-                this.bruteForceShortestPath(distanceMatrix)
-            })
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    //let allDestinations = data["destination_addresses"]
+                    //this.setState({ allDestinations })
+                    // here we update the allDestinations in the state
+                    // this replaces the generic names we type in with the specific addresses
+                    // for example, Washington gets replaced with Washington, USA
+                    // if we don't want the user interface to change, we can replace this with
+                    // a different variable no problem
+                    let distanceMatrix = data["rows"]
+                    console.log("DISTANCE MATRIX:\n" + distanceMatrix);
+                    this.bruteForceShortestPath(distanceMatrix, numSearch)
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        });
     }
 
     autocompleteText(text) {
@@ -221,6 +328,9 @@ export default class Home extends Component {
                 let autocomplete = data["predictions"]
                 this.setState({ autocomplete })
             })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 
     onPlaceChange(event, pos) {
@@ -262,30 +372,34 @@ export default class Home extends Component {
             return
         }
 
-        // if pos == 0
-        if (pos == 0) {
+        // // if pos == 0
+        // if (pos == 0) {
 
-            // check if app is set to start at current location
-            if (self.state.startAtCurrentLocation) {
+        //     // check if app is set to start at current location
+        //     if (self.state.startAtCurrentLocation) {
 
-                // this should, by default, uncheck the checkbox
-                self.setState({ startAtCurrentLocation: !self.state.startAtCurrentLocation});
-            }
-        }
+        //         // this should, by default, uncheck the checkbox
+        //         self.setState({ startAtCurrentLocation: !self.state.startAtCurrentLocation});
+        //     }
+        // }
 
         destinations.splice(pos, 1)
         this.setState({ destinations })
     }
     
-    onSubmit() {
+    onSubmit() {        
         // filter out all of the empty destinations
-        let filteredDestinations = this.state.destinations.filter(destination => destination.length > 0)
+        let filteredDestinations = this.state.destinations.filter(destination => destination.length > 0);
+        let allDestinations = filteredDestinations;
 
-        let allDestinations = filteredDestinations
-        //allDestinations.unshift(this.state.startingPoint)
-        //this.state.returnBackHome ? allDestinations.push(this.state.startingPoint) : allDestinations.push(this.state.endingPoint)
-        this.state.returnBackHome ? allDestinations.push(allDestinations[0]) : null
-        this.setState({ allDestinations }, () => this.getAllDistances()) // for distance matrix stuff
+
+
+        this.setState({permDestinations: []}, 
+                      () => this.setState({matrixDestinations: []}, 
+                        () => this.setState({ allDestinations }, 
+                            () => this.getAllDistances())));
+
+        // this.setState({ allDestinations }, () => this.getAllDistances()); // for distance matrix stuff
     }
     
     // populates final destination with starting destination at index 0 
